5cd5e6731549a899d02fdd8691ae2e4e30193b50
Nicolas Romanetti <nromanetti@macbook-pro-de-nicolas.home>
2016-06-01 23:26:27 +0200

    support LocalDateTime in p-calendar...

--- pack-angular/celerio/pack-angular/src/main/java/config/JacksonConfiguration.p.vm.java
+++ pack-angular/celerio/pack-angular/src/main/java/config/JacksonConfiguration.p.vm.java
@@ -16,19 +16,32 @@ $output.java("${configuration.rootPackage}.config","JacksonConfiguration")##
 
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
 @Configuration
 public class JacksonConfiguration {
+    public static final DateTimeFormatter LOCAL_DATE_TIME_FORMATTER = new DateTimeFormatterBuilder()
+        .parseCaseInsensitive()
+        .append(DateTimeFormatter.ISO_LOCAL_DATE)
+        .appendLiteral(' ') // instead of 'T'
+        .append(DateTimeFormatter.ISO_LOCAL_TIME)
+        .toFormatter();
 
     @Bean
     Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
         JavaTimeModule module = new JavaTimeModule();
-        return new Jackson2ObjectMapperBuilder()
-            .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-            .findModulesViaServiceLoader(true)
+        module.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(LOCAL_DATE_TIME_FORMATTER));
+        module.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(LOCAL_DATE_TIME_FORMATTER));
+
+        return new Jackson2ObjectMapperBuilder().featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS).findModulesViaServiceLoader(true)
             .modulesToInstall(module);
     }
 }
