01ee5df2e9abd1d3b28a4682b6c98508d38276f7
Nicolas Romanetti <nromanetti@macbook-pro-de-nicolas.home>
2016-06-20 23:49:26 +0200

    load only rxjs operator we use

--- pack-angular/celerio/pack-angular/src/main/webapp/app/boot.ts
+++ pack-angular/celerio/pack-angular/src/main/webapp/app/boot.ts
@@ -1,5 +1,5 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {AppComponent} from './app.component';
-import 'rxjs/Rx';
+import './rxjs-operators'; // Add the RxJS Observable operators we need in this app.
 
 bootstrap(AppComponent);
