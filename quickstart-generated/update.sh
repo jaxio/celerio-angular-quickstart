04a163b45b7803c3b7a35e4a0d6ed53b90bb12aa
Nicolas Romanetti <nromanetti@macbook-pro-de-nicolas.home>
2017-03-29 21:13:02 +0200

    script to update the content of quickstart-generated folder

---
+++ quickstart-generated/update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+rm -rf src web
+ng new web
+rm web/src/main.ts web/src/app/app.module.ts web/src/app/app.component.* web/src/styles.css
+mvn -Pdb,metadata,gen generate-sources
+rm WELCOME.TXT
+rm -r .celerio
+rm -r target
+cd web
+npm install --save @angular/animations
+npm install --save @angular/material
+npm install --save primeng@4.0.0-rc.1
+npm install --save font-awesome
+rm -r node_modules
+cd ..
+git add src web
+git commit -m "sync with latest templates"
+git push
