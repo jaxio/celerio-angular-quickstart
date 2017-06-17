24dcea6f28c644ece22359b238ac4bf99b8fbb01
Nicolas Romanetti <nromanetti@macbook-pro-de-nicolas.home>
2017-06-17 18:27:31 +0200

    fixes, see CHANGELOG

--- quickstart-generated/update.sh
+++ quickstart-generated/update.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 rm -rf src web
 ng new web
-rm web/src/main.ts web/src/app/app.module.ts web/src/app/app.component.* web/src/styles.css
+rm web/src/app/app.module.ts web/src/app/app.component.* web/src/styles.css
 mvn -Pdb,metadata,gen generate-sources
 rm WELCOME.TXT
 rm -r .celerio
 rm -r target
 cd web
 npm install --save @angular/animations
-npm install --save @angular/material@2.0.0-beta.3
-npm install --save primeng@4.0.0
+npm install --save @angular/material@2.0.0-beta.6
+npm install --save primeng@4.1.0-rc.2
 npm install --save font-awesome
 rm -r node_modules
 cd ..
