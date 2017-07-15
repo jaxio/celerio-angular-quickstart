8d63b62a6f005b00b7512e1c2c6ab3e8110e48f7
polinchw <polinchw@netscape.net>
2017-07-15 09:41:03 -0400

    Added a build.sh

--- quickstart/build.sh
+++ quickstart/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -rf web
 ng new web
 rm web/src/app/app.module.ts web/src/app/app.component.* web/src/styles.css
 mvn -Pdb,metadata,gen generate-sources
@@ -7,7 +8,7 @@ npm install --save @angular/animations
 npm install --save @angular/material@2.0.0-beta.7
 npm install --save primeng@4.1.0-rc.3
 npm install --save font-awesome
-#ng build --environment=production
-ng build
+ng build --prod
+cp dist/* ../src/main/resources/static
 cd ..
-docker build . -t polinchw/monitor-frontend:latest
+mvn package
