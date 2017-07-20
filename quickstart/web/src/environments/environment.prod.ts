984edf7a25b937e473ad6aaa194a8e3591b34e45
polinchakb <polinchakb@elnk.com>
2017-07-20 13:39:02 -0400

    Corrected profile name.

--- quickstart/web/src/environments/environment.prod.ts
+++ quickstart/web/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url: 'http://monitor-dev-slb-2083129347.us-east-1.elb.amazonaws.com'
+  url: 'https://monitor-dev-slb-2083129347.us-east-1.elb.amazonaws.com'
 };
 
 
