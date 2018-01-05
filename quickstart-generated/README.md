06fda691f4290b884d0db63c2a3fa18c5bc04ee2
Nicolas Romanetti <nromanetti@macbook-pro-de-nicolas.home>
2018-01-05 17:07:17 +0100

    clean up

--- quickstart-generated/README.md
+++ quickstart-generated/README.md
@@ -1,4 +1,4 @@
-# Angular 2 generated quickstart
+# Angular generated quickstart
 
 Here is the quickstart generated code so you can have a look at it with no effort :)
 
--- quickstart-generated/update.sh
+++ quickstart-generated/update.sh
@@ -7,10 +7,9 @@ rm WELCOME.TXT
 rm -r .celerio
 rm -r target
 cd web
-npm install --save @angular/animations
-npm install --save @angular/material@2.0.0-beta.6
-npm install --save primeng@4.1.0-rc.2
-npm install --save font-awesome
+npm i @angular/material @angular/cdk
+npm i primeng
+npm i font-awesome
 rm -r node_modules
 cd ..
 git add src web
